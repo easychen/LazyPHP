b8b63f4f63f81575b5b5f981f160885557e0513f
koboshi <koboshi1989@gmail.com>
2014-01-03 16:25:28 +0800

    入口文件include类库从相对路径改为绝对路径 include_once改为require 避免重复包含和嵌套引起bug

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ define( 'DS' , DIRECTORY_SEPARATOR );
 define( 'AROOT' , dirname( __FILE__ ) . DS  );
 
 //ini_set('include_path', dirname( __FILE__ ) . DS .'_lp' ); 
-include_once( '_lp'.DS .'lp.init.php' );
+//include_once( '_lp'.DS .'lp.init.php' );
+include AROOT . '_lp'.DS .'lp.init.php';
 /**** lp framework init finished ***/
